cfe48bd12c35f812dd17c60c723e02d30313c209
git-done <52246389+git-done@users.noreply.github.com>
2020-06-06 09:43:24 +0800

    Update deploy.groovy

--- src/org/devops/deploy.groovy
+++ src/org/devops/deploy.groovy
@@ -2,5 +2,10 @@ package org.devops
 
 //saltstack
 def SaltDeploy(hosts,func){
-	sh "salt \"${hosts}\" ${func}"
+    sh "salt \"${hosts}\" ${func}"
+}
+
+//ansible
+def AnsibleDeploy(hosts,func){
+	sh "ansible ${func} ${hosts}"
 }
